114a7bdafb80c7c083b2cdc9bf944873418bc3b8
BLACK-ASH <100258345+BLACK-ASH@users.noreply.github.com>
2025-10-01 20:30:48 +0530

    Auth and Navbar Updated- bug fix

--- app/(Admin)/orders/page.js
+++ app/(Admin)/orders/page.js
@@ -4,7 +4,7 @@ import { DataTable } from "./data-table"
 import OrderRefresh from '@/components/OrderRefresh';
 
 const OrderPage = async () => {
-    const orders = await getAllOrders();
+    // const orders = await getAllOrders();
     
 
     return (
@@ -12,7 +12,7 @@ const OrderPage = async () => {
             <h1 className='text-2xl font-bold'>Orders</h1>
             <OrderRefresh />
             <div className="container mx-auto py-10">
-                <DataTable columns={columns} data={orders} />
+                {/* <DataTable columns={columns} data={orders} /> */}
             </div>
         </div>
     )
